107563dd96ba0d9b2d6b3af206929e7c8c80cf32
FazleRahmanEvan <evanshakib26@gmail.com>
2025-02-17 00:14:39 +0600

    app context

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { createContext } from "react";
+import { doctors } from "../assets/assets";
+
+export const AppContext = createContext();
+
+const currencySymbol = "$";
+
+const AppContextProvider = (props) => {
+  const value = {
+    doctors,
+    currencySymbol,
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
