d846654dcdd7a26f9ea22c0a39ae75a2ec7befca
FazleRahmanEvan <evanshakib26@gmail.com>
2025-03-07 02:27:12 +0600

    appointment updtae

--- src/pages/MyAppointment.jsx
+++ src/pages/MyAppointment.jsx
@@ -132,31 +132,35 @@ const MyAppointments = () => {
             <div></div>
             <div className="flex flex-col gap-2 justify-end text-sm text-center">
               {!item.cancelled &&
-                item.payment &&
+                !item.payment &&
                 !item.isCompleted &&
                 payment !== item._id && (
-                  <button className="sm:min-w-48 py-2 border rounded text-[#696969]  bg-[#EAEFFF]">
-                    Paid
+                  <button
+                    onClick={() => setPayment(item._id)}
+                    className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300"
+                  >
+                    Pay Online
                   </button>
                 )}
-              {!item.cancelled && !item.payment && !item.isCompleted && (
-                <button
-                  onClick={() => appointmentStripe(item._id)}
-                  className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center"
-                >
-                  <img
-                    className="max-w-20 max-h-5"
-                    src={assets.stripe_logo}
-                    alt=""
-                  />
-                </button>
-              )}
-              {!item.cancelled && !item.payment && !item.isCompleted && (
-                <button
-                  onClick={() => setPayment(item._id)}
-                  className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300"
-                >
-                  Pay Online
+              {!item.cancelled &&
+                !item.payment &&
+                !item.isCompleted &&
+                payment === item._id && (
+                  <button
+                    onClick={() => appointmentStripe(item._id)}
+                    className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-gray-100 hover:text-white transition-all duration-300 flex items-center justify-center"
+                  >
+                    <img
+                      className="max-w-20 max-h-5"
+                      src={assets.stripe_logo}
+                      alt=""
+                    />
+                  </button>
+                )}
+
+              {!item.cancelled && item.payment && !item.isCompleted && (
+                <button className="sm:min-w-48 py-2 border rounded text-[#696969]  bg-[#EAEFFF]">
+                  Paid
                 </button>
               )}
 
@@ -166,7 +170,7 @@ const MyAppointments = () => {
                 </button>
               )}
 
-              {item.cancelled && !item.isCompleted && (
+              {!item.cancelled && !item.isCompleted && (
                 <button
                   onClick={() => cancelAppointment(item._id)}
                   className="text-[#696969] sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300"
